03463efa31818cc53d15ebc7e53aba495fd75562
AkCzx <nathan19952008@gmail.com>
2021-04-28 23:27:30 +0800

    Fix props

--- src/components/Marketplace/DetailTitlePrice.js
+++ src/components/Marketplace/DetailTitlePrice.js
@@ -19,6 +19,7 @@ function DetailTitlePrice(props) {
           Buy Now (LAUNCH MODAL)
         </Button>
         <DetailBuyModal
+          itemdata={props.itemdata}
           show={showBuyModal}
           onHide={() => setShowBuyModal(false)}
         />
