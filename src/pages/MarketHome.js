6a9350f9c1da0b0db84c21e6eb67d5c226060c86
AkCzx <nathan19952008@gmail.com>
2021-04-30 15:03:58 +0800

    HomeFeatureCards

--- src/pages/MarketHome.js
+++ src/pages/MarketHome.js
@@ -2,15 +2,28 @@ import Navi from "../components/Common/Navbar";
 import HomeNewlyMinted from "../components/Marketplace/HomeNewlyMinted";
 import classes from "./MarketHome.module.css";
 import "../App.css";
+import HomeFeatureCard from "../components/Marketplace/HomeFeatureCard";
+import { Container, Row, Col } from "react-bootstrap";
 
 function MarketHome() {
   return (
     <>
       <Navi />
       {/* <Trending Collection /> */}
-      {/* <Maybe a Carousel /> */}
-      {/* SOME CARD */}
+      {/* <Maybe a Carousel for collection/> */}
+
       <div className={classes.markethome}>
+        <Container fluid>
+          <Row>
+            <Col className="pl-0">
+              <HomeFeatureCard />
+            </Col>
+            <Col className="pr-0">
+              <HomeFeatureCard />
+            </Col>
+          </Row>
+        </Container>
+        <HomeFeatureCard />
         <div className={classes.title}>
           <h5>Trending Items</h5>
         </div>
