f0bbdc680cc35bcb0551c6be2a36014a587c4fdc
Philemonip <philemonip@gmail.com>
2021-05-03 16:31:10 +0800

    3 May update

--- migrations/2_fivetoken_migration.js
+++ migrations/2_fivetoken_migration.js
@@ -1,13 +1,11 @@
-const FiveToken = artifacts.require("FiveToken");
 const Token = artifacts.require("./Token");
 const Banco = artifacts.require("./Banco");
-const CloseSeaNFT = artifacts.require("CloseSeaNFT")
+const CloseSeaNFT = artifacts.require("CloseSeaNFT");
 module.exports = async function (deployer) {
-	await deployer.deploy(FiveToken);
-	await deployer.deploy(CloseSeaNFT);
-	await deployer.deploy(Token);
-	const token = await Token.deployed();
-	await deployer.deploy(Banco, token.address);
-	const banco = await Banco.deployed();
-	await token.passMinterRole(banco.address);
+  await deployer.deploy(CloseSeaNFT);
+  await deployer.deploy(Token);
+  const token = await Token.deployed();
+  await deployer.deploy(Banco, token.address);
+  const banco = await Banco.deployed();
+  await token.passMinterRole(banco.address);
 };
