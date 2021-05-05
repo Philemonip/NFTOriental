e37251ad3c148f0f8941cc53ca4a7dad9c650cfd
Chan Tsz Him Oscar <Oscar@wailuntiiPhone2.localdomain>
2021-05-05 19:21:45 +0800

    transaction history completed

--- src/components/Marketplace/Detail/DetailTitlePrice.js
+++ src/components/Marketplace/Detail/DetailTitlePrice.js
@@ -7,7 +7,6 @@ import DetailBuyModal from "./DetailBuyModal";
 
 function DetailTitlePrice(props) {
   const [showBuyModal, setShowBuyModal] = useState(false);
-  const [itemName, setitemName] = useState("");
   // const token = useSelector((state) => state.detail.token);
 
   return (
@@ -19,7 +18,7 @@ function DetailTitlePrice(props) {
         <p>Current Price</p>
         <p className={classes.title}>ETH {props.itemdata.current_price}</p>
         <Button variant="primary" onClick={() => setShowBuyModal(true)}>
-          Buy Now (LAUNCH MODAL)
+          Buy Now
         </Button>
         <DetailBuyModal
           itemdata={props.itemdata}
@@ -28,30 +27,6 @@ function DetailTitlePrice(props) {
           token_id={props.token_id}
           onHide={() => setShowBuyModal(false)}
         />
-
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            console.log(itemName);
-            props.mint(itemName);
-            setitemName("");
-          }}
-        >
-          <div className="form-group">
-            <input
-              id="itemName"
-              type="text"
-              value={itemName}
-              onChange={(e) => setitemName(e.target.value)}
-              className="form-control form-control-md"
-              placeholder="Item Name"
-              required
-            />
-          </div>
-          <Button type="submit" className="m-3" variant="success">
-            Mint
-          </Button>
-        </form>
       </div>
     </>
   );
