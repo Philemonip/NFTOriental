cb6c57caff2adec84defa5b0262ef472e857ac88
Philemonip <philemonip@gmail.com>
2021-05-07 13:40:32 +0800

    update

--- src/redux/Marketplace/detailSlice.js
+++ src/redux/Marketplace/detailSlice.js
@@ -1,38 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    web3: "undefined",
-    owner: "",
-    contract: null,
-    items: [],
-    token: [],
+  web3: "undefined",
+  owner: "",
+  contract: null,
+  items: [],
+  token: [],
 };
 
 const detailSlice = createSlice({
-    name: "detail",
-    initialState,
-    reducers: {
-        updateWeb3(state, action) {
-            state.web3 = action.payload;
-        },
-        updateCurrentUser(state, action) {
-            state.currentUser = action.payload;
-        },
-        updateContract(state, action) {
-            state.contract = action.payload;
-        },
-        updateItem(state, action) {
-            state.items = action.payload;
-        },
-        updateToken(state, action) {
-            state.token = action.payload;
-        },
-    }
+  name: "detail",
+  initialState,
+  reducers: {
+    updateWeb3(state, action) {
+      state.web3 = action.payload;
+    },
+    updateCurrentUser(state, action) {
+      state.currentUser = action.payload;
+    },
+    updateContract(state, action) {
+      state.contract = action.payload;
+    },
+    updateItem(state, action) {
+      state.items = action.payload;
+    },
+    updateToken(state, action) {
+      state.token = action.payload;
+    },
+  },
 });
 
 export const detailSliceActions = detailSlice.actions;
 
 export default detailSlice;
-
-
-
