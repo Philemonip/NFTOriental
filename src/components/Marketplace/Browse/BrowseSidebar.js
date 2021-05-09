3e1c545d3fabec8519a844c22d55ee280925c5a3
nathan19952008@gmail.com <nathan19952008@gmail.com>
2021-05-10 01:55:42 +0800

    Fixed sidebar

--- src/components/Marketplace/Browse/BrowseSidebar.js
+++ src/components/Marketplace/Browse/BrowseSidebar.js
@@ -3,14 +3,16 @@ import classes from "./BrowseSidebar.module.css";
 import SidebarCollection from "../../Common/Sidebar/SidebarCollection";
 import SidebarSort from "../../Common/Sidebar/SidebarSort";
 import SidebarStatus from "../../Common/Sidebar/SidebarStatus";
+// import { IoIosArrowUp, IoIosArrowDown } from "react-icons/io";
+// import { HiOutlineCollection } from "react-icons/hi";
 
 function BrowseSidebar() {
   const accordionArr = [
     {
       name: "Status",
-      component: <SidebarStatus isSeller="false" />,
+      component: <SidebarStatus isSeller={false} />,
     },
-    { name: "Sort by", component: <SidebarSort isSeller="false" /> },
+    { name: "Sort by", component: <SidebarSort isSeller={false} /> },
     { name: "Collections", component: <SidebarCollection isSeller={false} /> },
   ];
 
@@ -21,6 +23,8 @@ function BrowseSidebar() {
           <Accordion defaultActiveKey="0" key={key}>
             <Card className={classes.card}>
               <Accordion.Toggle as={Card.Header} eventKey="0">
+                {/* <HiOutlineCollection /> */}
+                {i.icon}
                 {i.name}
               </Accordion.Toggle>
               <Accordion.Collapse eventKey="0">
