d89265897d5336cf7e2127775218cb567b6035e0
Philemonip <philemonip@gmail.com>
2021-05-13 19:27:50 +0800

    update

--- src/pages/MarketDetail.js
+++ src/pages/MarketDetail.js
@@ -24,6 +24,8 @@ var web3;
 var cch;
 var contractNFT;
 
+console.log(CloseSeaNFT.networks);
+
 function MarketDetail() {
   const params = useParams();
   const [item, setItems] = useState("");
@@ -138,7 +140,7 @@ function MarketDetail() {
         //need another dispatch here
       } catch (err) {
         console.log("buying error", err);
-        closeModelClearHash()
+        closeModelClearHash();
       }
     } else {
       window.alert(
@@ -170,7 +172,8 @@ function MarketDetail() {
           .on("transactionHash", async function (hash) {
             console.log("hash on(transactionHash nft " + hash);
             dispatch(detailSliceActions.updateNftHash(hash));
-          }).then(async () => {
+          })
+          .then(async () => {
             const getItem = await contractNFT.methods.getAllItems().call();
             await dispatch(detailSliceActions.updateItem(getItem));
             console.log(getItem, "please get this item");
@@ -191,13 +194,15 @@ function MarketDetail() {
                 on_sale: false,
               })
             );
-            closeModelClearHash()
-          }).catch(() => {
-            window.alert("")
-            closeModelClearHash()
+            closeModelClearHash();
+          })
+          .catch(() => {
+            window.alert("");
+            closeModelClearHash();
           });
-      }).catch(() => {
-        closeModelClearHash()
+      })
+      .catch(() => {
+        closeModelClearHash();
       });
   }
   async function closeModelClearHash() {
@@ -249,11 +254,11 @@ function MarketDetail() {
           on_sale: forSale,
         })
       );
-      closeModelClearHash()
+      closeModelClearHash();
       // window.location.reload();
     } catch (err) {
       console.log("item on sale error", err);
-      closeModelClearHash()
+      closeModelClearHash();
     }
   }
 
@@ -282,10 +287,10 @@ function MarketDetail() {
           on_sale: forSale,
         })
       );
-      closeModelClearHash()
+      closeModelClearHash();
     } catch (err) {
       console.log("item not for sale error", err);
-      closeModelClearHash()
+      closeModelClearHash();
     }
   }
   /////////////////////////////////////////////////////////////////////////
